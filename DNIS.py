f771c8480f0579a719ad914eea8b6e7196e8d125
lamnguyen <43162568+lam-nguyenv@users.noreply.github.com>
2019-05-11 21:13:11 +0700

    Update DNIS.py

--- DNIS.py
+++ DNIS.py
@@ -1,6 +1,20 @@
 import networkx as nx
 from createGraph import createGraph
 
+'''
+# Graph Attributes:
+Node: (Not done):
+    osmid
+    x: Coordinate
+    y: Coordinate
+    tag: Type
+Edge: 
+    osmid
+    highway: Road type
+    oneway: True - False
+    length: float
+'''
+
 def dnis():
     G = createGraph()
-    
+
