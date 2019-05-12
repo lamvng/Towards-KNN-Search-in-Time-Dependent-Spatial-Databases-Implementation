7f55c72a15ba0c32408be546a66f6bd209a82f35
lamnguyen <43162568+lam-nguyenv@users.noreply.github.com>
2019-05-12 22:20:55 +0700

    Add files via upload

--- DNIS.py
+++ DNIS.py
@@ -5,18 +5,17 @@ from createGraph import createGraph
 # Các nhãn của đồ thị:
 Vertice: (Đang làm):
     osmid
-    x: Toạ độ x
-    y: Toạ độ y
-    amenity: Nơi công cộng. Giá trị có thể là hospital, park,...
-    shop: Cửa hàng. Giá trị là tên cửa hàng đó.
-    name: Tên địa điểm
-    address: Địa chỉ
+    lat: Latitude.
+    lon: Longtitude.
+    type: Kiểu địa điểm. Giá trị là bank, hospital, park, shop...
+    name: Tên.
+    address: Địa chỉ.
     # Trong trường hợp vertice là các giao lộ hoặc ngã tư, thì chỉ có 3 nhãn osmid, x, y
 Edge:
     osmid
-    highway: Loại đường
-    oneway: True - False
-    length: float
+    highway: Loại đường.
+    oneway: True - False.
+    length: float.
 '''
 
 def dnis():
--- createGraph.py
+++ createGraph.py
@@ -2,36 +2,48 @@ import osmnx as ox
 import networkx as nx
 import xml.etree.ElementTree as et
 
+
 # Parse all nodes in OSM file
+# Return a list, whose element is a node dictionary: osmid, x, y, type, name, address
 def parseXML():
     node_list = []
-    data = et.parse('.//map//test.osm')
+    data = et.parse('.//map//hanoi_bk.osm')
     root = data.getroot()
-    for node in root.findall('.//osm//node'):
+    for node in root.findall('./node'):
         node_dict = dict()
-        node_dict.fromkeys(['id', 'x', 'y', 'address', 'tag'])
+        node_dict.fromkeys(['id', 'lat', 'lon', 'type', 'name' 'address'])
         id = int(node.attrib['id'])
-        x = node.attrib['x']
-        y = node.attrib['y']
+        lat = node.attrib['lat']
+        lon = node.attrib['lon']
+        node_dict['id'] = id
+        node_dict['lat'] = lat
+        node_dict['lon'] = lon
         for tag in root.findall('.//osm//node//tag'):
-                tag_dict = dict()
-                tag_dict.fromkeys(['amenity', 'shop', 'name', 'address'])
-                if tag.find("field[@k='amenity']") is not None:
-                        amenity = tag.find("field[@k='amenity']").attrib['v']
-                        tag_dict['amenity'] = amenity
-                if tag.find("field[@k='shop']") is not None:
-                        shop = tag.find("field[@k='shop']").attrib['v']
-                        tag_dict['shop'] = shop
-                if tag.find("field[@k='name']") is not None:
-                        name = tag.find("field[@k='name']").attrib['v']
-                        tag_dict['name'] = name
-                if tag.find("field[@k='addr:street']") is not None:
-                        address = tag.find("field[@k='addr:street']").attrib['v']
-                        tag_dict['address'] = address
+            amenity_value = tag.find("field[@k='amenity']").attrib['v']
+            shop_value = tag.find("field[@k='shop']").attrib['v']
+            leisure_value = tag.find("field[@k='leisure']").attrib['v']
+            name_value = tag.find("field[@k='name']").attrib['v']
+            addr_value = tag.find("field[@k='addr:street']").attrib['v']
+            if amenity_value is not None:
+                node_dict['type'] = amenity_value
+            elif shop_value is not None:
+                node_dict['type'] = shop_value
+            elif leisure_value is not None:
+                node_dict['type'] = leisure_value
+            if name_value is not None:
+                node_dict['name'] = name_value
+            if addr_value is not None:
+                node_dict['address'] = addr_value
+        node_list.append(node_dict)
+    return node_list
 
-
-                
 # Create graph with node type
+
 def createGraph():
-    G = ox.graph_from_file('.//map//hanoi_bk.osm')
+    node_list = parseXML()
+    osm = ox.graph_from_file('.//map//hanoi_bk.osm') # Return a networkx graph
+    G = osm
     return G
+
+
+node_list = parseXML()
