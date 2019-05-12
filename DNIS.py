b0c1f203573864adcaac92bbb44eda66c35475bf
lamnguyen <43162568+lam-nguyenv@users.noreply.github.com>
2019-05-12 16:49:52 +0700

    Add files via upload

--- DNIS.py
+++ DNIS.py
@@ -2,15 +2,18 @@ import networkx as nx
 from createGraph import createGraph
 
 '''
-# Graph Attributes:
-Node: (Not done):
+# Các nhãn của đồ thị:
+Vertice: (Đang làm):
     osmid
-    x: Coordinate
-    y: Coordinate
-    tag: Type
-Edge: 
+    x: Toạ độ x
+    y: Toạ độ y
+    amenity: Nơi công cộng. Giá trị có thể là hospital, park,...
+    shop: Cửa hàng. Giá trị là tên cửa hàng đó.
+    name: Tên địa điểm
+    # Trong trường hợp vertice là các giao lộ hoặc ngã tư, thì chỉ có 3 nhãn osmid, x, y
+Edge:
     osmid
-    highway: Road type
+    highway: Loại đường
     oneway: True - False
     length: float
 '''
--- createGraph.py
+++ createGraph.py
@@ -1,7 +1,35 @@
 import osmnx as ox
 import networkx as nx
-import os
+import xml.etree.ElementTree as et
 
+# Parse all nodes in OSM file
+def parseXML():
+    node_list = []
+    data = et.parse('.//map//test.osm')
+    root = data.getroot()
+    for node in root.findall('.//osm//node'):
+        node_dict = dict()
+        node_dict.fromkeys(['id', 'x', 'y', 'address', 'tag'])
+        id = int(node.attrib['id'])
+        x = node.attrib['x']
+        y = node.attrib['y']
+        for tag in root.findall('.//osm//node//tag'):
+                tag_dict = dict()
+                tag_dict.fromkeys(['amenity', 'shop', 'name'])
+                if tag.find("field[@k='amenity']") is not None:
+                        amenity = tag.find("field[@k='amenity']").attrib['v']
+                        tag_dict['amenity'] = amenity
+                if tag.find("field[@k='shop']") is not None:
+                        shop = tag.find("field[@k='shop']").attrib['v']
+                        tag_dict['shop'] = shop
+                if tag.find("field[@k='name']") is not None:
+                        name = tag.find("field[@k='name']").attrib['v']
+                        tag_dict['name'] = name
+
+
+
+                
+# Create graph with node type
 def createGraph():
-    G = ox.graph_from_file('.\map\hanoi_bk.osm')
+    G = ox.graph_from_file('.//map//hanoi_bk.osm')
     return G
